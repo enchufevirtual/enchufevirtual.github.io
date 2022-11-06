ed5d6485c6b1d1d1dbe1878e307507eb6b273304
chendo <chendo@chendo>
2022-11-06 17:36:02 -0500

    Fixed Styles

--- src/components/Header/HeaderSocial.jsx
+++ src/components/Header/HeaderSocial.jsx
@@ -8,8 +8,8 @@ const HeaderSocial = () => {
 
  return (
   <ul className={active ? "Header_social transform" : "Header_social"}>
-    <li className="menu-link"><a href="https://wwww.instagram.com/enchufevirtual" target="_blank">Instagram</a></li>
-    <li className="menu-link"><a href="https://wwww.facebook.com/enchufevirtual" target="_blank">Facebook</a></li>
+    <li className="menu-link"><a href="https://www.instagram.com/enchufevirtual" target="_blank">Instagram</a></li>
+    <li className="menu-link"><a href="https://www.facebook.com/enchufevirtual" target="_blank">Facebook</a></li>
     <li className="menu-link"><a href="mailto:chendoec@live.com">Contact</a></li>
   </ul>
  ); 
