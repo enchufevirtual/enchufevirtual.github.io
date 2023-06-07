056ab5c0570d0c24f833a6a69253fe590aa46a27
chendo <chendoec@live.com>
2023-06-07 13:52:27 -0500

    Updated Hero Styles & Grid Styles Cards

--- src/components/Header/HeaderSocial.jsx
+++ src/components/Header/HeaderSocial.jsx
@@ -8,9 +8,9 @@ const HeaderSocial = () => {
 
  return (
   <ul className={active ? "Header_social transform" : "Header_social"}>
-    <li className="menu-link"><a href="https://github.com/orgs/enchufevirtual/repositories" target="_blank">GitHub</a></li>
-    <li className="menu-link"><a href="https://www.facebook.com/enchufevirtual" target="_blank">Facebook</a></li>
-    <li className="menu-link"><a href="https://radio.enchufevirtual.com" target="_blank">Radio</a></li>
+    <li className="menu-link"><a aria-label="repositories" href="https://github.com/orgs/enchufevirtual/repositories" target="_blank">GitHub</a></li>
+    <li className="menu-link"><a aria-label="facebook" href="https://www.facebook.com/enchufevirtual" target="_blank">Facebook</a></li>
+    <li className="menu-link"><a href="https://radio.enchufevirtual.com" aria-label="radio">Radio</a></li>
     <li>
       <select onChange={changeLanguage} value={language}  className="language">
         <option className="en" value="en">English</option>
--- webpack.config.js
+++ webpack.config.js
@@ -4,72 +4,73 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+  entry: './src/index.js',
 
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        clean: true
-    },
-    mode: 'development',
-    resolve: {
-        extensions: ['.tsx', '.jsx', '.ts', '.js'],
-        alias: {
-            '@': path.resolve(__dirname, './src'), // alias global
-            '@images': path.resolve(__dirname, './public/assets/'),
-            '@hooks': path.resolve(__dirname, './hooks/'),
-            '@public': path.resolve(__dirname, './public/'),
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader',
-                }
-            },
-            {
-                test: /\.s?css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'src/public/[name].[hash][ext]'
-                }
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-                type: 'asset/inline',
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html',
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin(),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "src/public", "assets"),
-                    to: "assets"
-                }
-            ]
-        }),
-    ],
-    devServer: {
-        historyApiFallback: true
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    clean: true,
+    publicPath: '/'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.jsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, './src'), // alias global
+      '@images': path.resolve(__dirname, './public/assets/'),
+      '@hooks': path.resolve(__dirname, './hooks/'),
+      '@public': path.resolve(__dirname, './public/'),
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+            loader: 'html-loader',
+        }
+      },
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+            filename: 'public/[name].[hash][ext]'
+        }
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: 'asset/inline',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public", "assets"),
+          to: "assets"
+        }
+      ]
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
 }
