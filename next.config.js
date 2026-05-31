1cdd1c6408961980109a7fa5792263b50e8e776c
soychendo <chendoec@live.com>
2026-05-31 00:20:12 -0500

    Fixed unread styles and unrecognized files by nexts.js and the GitHub Pages server

--- next.config.js
+++ next.config.js
@@ -1,33 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { parse } = require('url')
-const pkg = require('./package.json')
-
-function getBasePath() {
-  if (process.env.NEXT_PUBLIC_BASE_PATH) return process.env.NEXT_PUBLIC_BASE_PATH
-
-  // try to infer from package.json homepage, e.g. https://user.github.io/repo
-  if (pkg && pkg.homepage) {
-    try {
-      const u = parse(pkg.homepage)
-      if (u.pathname && u.pathname !== '/') return u.pathname.replace(/\/$/, '')
-    } catch (e) {
-      // ignore
-    }
-  }
-
-  return undefined
-}
-
-const inferredBasePath = getBasePath()
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  basePath: inferredBasePath,
-  assetPrefix: inferredBasePath,
-  turbopack: {
-    root: __dirname,
+  images: {
+    unoptimized: true,
   },
 }
 
